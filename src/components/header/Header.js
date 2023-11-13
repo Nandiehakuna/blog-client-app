4a647f318ffb1996159089c88a6f273132a4952b
Nandiehakuna <gakiikelly403@gmail.com>
2023-11-13 19:15:23 +0300

    creation of folders

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header =()=>{
+    return(
+        <section>
+
+            
+        </section>
+
+    )
+}
+export default Header;
---
+++ src/pages/layout/basicLayout/BasicLayout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const BasicLayout =()=>{
+
+return(
+    <section>
+        
+
+
+
+    </section>
+
+)
+
+
+
+
+}
+export default BasicLayout;
